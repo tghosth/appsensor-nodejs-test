0ec4c5f2623c4c0bbff91118f09d94afb8759c5a
Josh Grossman <tghosth@users.noreply.github.com>
2018-07-06 16:29:59 +0300

    Using the new logic

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 var appsensornodejs = require("../appsensor-nodejs");
+var points =  appsensornodejs.DetectionPoints
+
+
 
 console.log(appsensornodejs.HelloWorld("Josh"));
 
 console.log(appsensornodejs.AppSensorURL());
 
-for (i=1; i<=100; i++)
+for (i=1; i<=9; i++)
 {
-    appsensornodejs.SendEvent("fred","Input Validation", `IE0${i}`);
+    for (j=1; j<=10; j++)
+    {
+        //appsensornodejs.SendEvent("fred","Input Validation", `IE0${i}`);
+        appsensornodejs.SendEventByDP("fred", points.IE1)
+    }
 }
 
 
