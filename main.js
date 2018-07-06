1ca1caa8fdb113ee2e92f7b65d6ce0ec4d4719d5
Josh Grossman <tghosth@users.noreply.github.com>
2018-07-06 17:30:50 +0300

    More tidying/refactoring

--- main.js
+++ main.js
@@ -12,8 +12,7 @@ for (i=1; i<=9; i++)
 {
     for (j=1; j<=10; j++)
     {
-        //appsensornodejs.SendEvent("fred","Input Validation", `IE0${i}`);
-        appsensornodejs.SendEventByDP("fred", points.IE1)
+        appsensornodejs.SendEvent("fred", points.IE1)
     }
 }
 
