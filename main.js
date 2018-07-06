66629ba23629fb02d1d6039bb1f448b037be3c93
Josh Grossman <tghosth@users.noreply.github.com>
2018-07-06 12:44:37 +0300

    create basic test

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-var appsensornodejs = require("appsensor-nodejs")
+var appsensornodejs = require("../appsensor-nodejs");
 
-console.log(appsensornodejs.HelloWorld("Josh"))
+console.log(appsensornodejs.HelloWorld("Josh"));
 
-process.exit()
+console.log(appsensornodejs.AppSensorURL());
+
+for (i=1; i<=100; i++)
+{
+    appsensornodejs.SendEvent("fred","Input Validation", `IE0${i}`);
+}
+
+
+//process.exit()
