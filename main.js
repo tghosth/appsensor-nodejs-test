f81274319dba034d09889c140d24ac034e3a08d8
Josh Grossman <tghosth@users.noreply.github.com>
2018-07-08 09:40:34 +0300

    Stagger streaming and pull response

--- main.js
+++ main.js
@@ -2,21 +2,42 @@ var appsensornodejs = require('../appsensor-nodejs');
 //var appsensornodejs = require('appsensor-nodejs');
 var points =  appsensornodejs.DetectionPoints
 
-
+var responseCallback = function (error, response, body) 
+    {
+        if (error)
+        {
+            console.log(error);
+            
+        }
+        else
+        {
+            console.log(`ResponseCode: ${response && response.statusCode} Response: ${body}`);
+        }
+    
+    };
 
 console.log(appsensornodejs.HelloWorld("Josh"));
 
 console.log(appsensornodejs.AppSensorURL());
 
-for (i=1; i<=9; i++)
+for (i=1; i<=30; i++)
 {
-    console.log(appsensornodejs.SendEvent("fred", points.IE1));
+    setTimeout(function() {
+        console.log(appsensornodejs.SendEvent("fred", points.IE1));
+    }, i*1000);
 }
 
-for (j=1; j<=10; j++)
+for (j=1; j<=30; j++)
 {
-    console.log(appsensornodejs.SendEvent("bob", points.AE4));
+    setTimeout(function() {
+        console.log(appsensornodejs.SendEvent("bob", points.AE4));
+    }, j*1500);
 }
+
+
+setTimeout(function() {
+    appsensornodejs.GetResponses(responseCallback);
+}, 60000);
 
 
 //process.exit()
